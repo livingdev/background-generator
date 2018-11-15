853df3afc389fe82963fa3ab9a59e2c7ba5c424a
LivingDev <41175807+LivingDev@users.noreply.github.com>
2018-11-15 15:12:37 +0100

    index.html and script.js modified

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+//let livingdev = "changes through git"
 
 function setGradient() {
 	body.style.background = 
